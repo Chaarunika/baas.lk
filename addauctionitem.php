3495e3178031deb4a6bb775c89fc9042c8d8a7c6
malith <malith009@gmail.com>
2015-11-14 13:22:50 +0530

    changed auction

--- addauctionitem.php
+++ addauctionitem.php
@@ -8,11 +8,25 @@
 
 <style type="text/css">
 
+#apDiv1 {
+	position:absolute;
+	width:750px;
+	height:700px;
+	z-index:1;
+	left: 25%;
+	top: 116px;
+	
+	background-color: #f0f0f0;
+	border-radius: 20px;
+	border:1px solid rgba(51,51,51,0.5)
+}
+
+
 #apDiv2 {
 	position:absolute;
 	width:511px;
 	height:84px;
-	z-index:1;
+	z-index:2;
 	left: 507px;
 	top: 116px;
 }
@@ -20,7 +34,7 @@
 	position:absolute;
 	width:597px;
 	height:115px;
-	z-index:2;
+	z-index:3;
 	left: 506px;
 	top: 325px;
 }
@@ -45,7 +59,7 @@
 
 	color: #213266;
     font-size: 1em;
-    border: 5px solid #FFBF00;
+  
     padding: 3px 7px 2px 7px;
 }
 
@@ -64,11 +78,16 @@
     background-color: #;
 }
 
+p{ font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+    font-size: 20px;
+	color: #213266;
+	font-weight:bold;}
+
 
 
 </style>
 </head>
-<body style="background-color:#F5DA81">
+<body>
 
 <?php
 include ('header.php') ;
@@ -77,21 +96,21 @@ include ('header.php') ;
 
 ?>
 
+<div id="apDiv1" >
 
-<div id="apDiv2" >
 <h2>Adding Land</h2>
-<p >Please add the details about the land that you wish to sell</p>
-</div>
+<p >Please add the details about the land that you wish to sell...</p>
 
 
-<div id="apDiv3">
+
+
 <form id="form1" name="form1" method="post" action="addauctionprocess.php">
 
 <table id="bidtbl"  >
 
 <tr>
-<td width="550">Land Name</td>
-<td width="500"><input type="text" name="item" id="item" maxlength="8000"></td>
+<td ><p>Land Name</p></td>
+<td ><input type="text" name="item" id="item" style="width:280px"></td>
 
 </tr>
 
@@ -99,17 +118,49 @@ include ('header.php') ;
 
 
 <tr>
-<td valign="top">Description</td>
-<td><textarea name="description" id="description" cols="45" rows="5"></textarea> </td>
+<td valign="top"><p>Description</p></td>
+<td><textarea name="description" id="description" cols="45" rows="10"></textarea> </td>
 </tr>
 
-<tr><td width="550">Town</td>
-<td width="550"><input type="text" name="town" id="item" maxlength="8000"></td>
+<tr><td><p>Town</p></td>
+<td><select name=Town>
+
+
+  <option value="Colombo">Colombo</option>
+  <option value="Kandy">Kandy</option>
+  <option value="Galle">Galle</option>
+  <option value="Ampara">Ampara</option>
+  <option value="Anuradhapura">Anuradhapura</option>
+  <option value="Badulla">Badulla</option>
+  <option value="Batticaloa">Batticaloa</option>
+  <option value="Gampaha">Gampaha</option>
+  <option value="Hambantota">Hambantota</option>
+  <option value="Jaffna">Jaffna</option>
+  <option value="Kalutara">Kalutara</option>
+  <option value="Kegalle">Kegalle</option>
+  <option value="Kilinochchi">Kilinochchi</option>
+  <option value="Kurunegala">Kurunegala</option>
+  <option value="Mannar">Mannar</option>
+  <option value="Matale">Matale</option>
+  <option value="Matara">Matara</option>
+  <option value="Moneragala">Moneragala</option>
+  <option value="Mullativu">Mullativu</option>
+  <option value="Nuwara Eliya">Nuwara Eliya</option>
+  <option value="Polonnaruwa">Polonnaruwa</option>
+  <option value="Puttalam">Puttalam</option>
+  <option value="Ratnapura">Ratnapura</option>
+  <option value="Trincomalee">Trincomalee</option>
+  <option value="Vavuniya">Vavuniya</option>
+  
+
+</select>
+
+</td>
 </tr>
 
 
 
-<tr><td>Add Picture</td>
+<tr><td><p>Add Picture</p></td>
 <td><input  name="uploadedimage" enctype="multipart/form=data" class="element file" type="file"/></td>
 </tr>
 
@@ -118,8 +169,8 @@ include ('header.php') ;
 
 
 
-<tr><td>Bid Closing Time</td>
-<td> <input name="closingtime" id="demo3" size="25" width="100">
+<tr><td><p>Bid Closing Time</p></td>
+<td> <input name="closingtime" id="demo3" size="25" width="280">
 <a href="javascript:NewCal('demo3','mmmddyyyy',true,12)"><img src="pics/cal.gif" width="50" height="50" border="0" alt="Pick a date"></a></td>
 <!--<td>&nbsp;</td>-->
 </tr>
@@ -134,6 +185,7 @@ include ('header.php') ;
 
 
 </form>
+
 
 
 
