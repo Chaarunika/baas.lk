fd74366ebc63a54474ec46ede4681643aca7477a
malith <malith009@gmail.com>
2016-01-03 16:57:34 +0530

    rating widget added

--- profile.php
+++ profile.php
@@ -65,7 +65,51 @@
 	$sp = mysqli_fetch_assoc($result2);
 	
 	?>
-  
+
+
+<?php 
+echo "
+<script type=\"text/javascript\">(function(d, t, e, m){    
+    
+    window.RW_Async_Init = function(){
+                
+        RW.init({
+            huid: \"".$user['user_id']."\",
+            uid: \"5c4f6f81191890288de05d59c79ac9ea\",
+            source: \"website\",
+            options: {
+                \"advanced\": {
+                    \"font\": {
+                        \"size\": \"14px\"
+                    },
+                    \"text\": {
+                        \"rateAwful\": \"Not Good\",
+                        \"rateThis\": \"Rate\"
+                    }
+                },
+                \"size\": \"medium\",
+                \"style\": \"oxygen1\",
+                \"isDummy\": false
+            } 
+        });
+        RW.render();
+    };
+        // Append Rating-Widget JavaScript library.
+    var rw, s = d.getElementsByTagName(e)[0], id = \"rw-js\",
+        l = d.location, ck = \"Y\" + t.getFullYear() + 
+        \"M\" + t.getMonth() + \"D\" + t.getDate(), p = l.protocol,
+        f = ((l.search.indexOf(\"DBG=\") > -1) ? \"\" : \".min\"),
+        a = (\"https:\" == p ? \"secure.\" + m + \"js/\" : \"js.\" + m);
+    if (d.getElementById(id)) return;              
+    rw = d.createElement(e);
+    rw.id = id; rw.async = true; rw.type = \"text/javascript\";
+    rw.src = p + \"//\" + a + \"external\" + f + \".js?ck=\" + ck;
+    s.parentNode.insertBefore(rw, s);
+    }(document, new Date(), \"script\", \"rating-widget.com/\"));
+</script> ";
+
+?>
+
 <div id="fullscreen">
 	<div id="fullscreen-inner">
 		<div id="fullscreen-inner-left" class="fullscreen-inner-button"><span class="icon-caret-left"></span></div>
@@ -104,6 +148,8 @@
         <p>member since : <?php if(isset($user['user_registration_datetime'])) { echo $user['user_registration_datetime'] ;} ?></p> 
         <p>Catagory : <?php if(isset($sp['category'])) { echo $sp['category'] ;} ?></p>
         <p>Area : <?php if(isset($sp['area'])) { echo $sp['area'] ;} ?></p>  
+
+        <div class="rw-ui-container"></div> 
         </div>
       	<!--
       	<div id="apDivProfPic2"><img class="img-circle2"  src= " <?php// echo $_SESSION['url'] ?> "  > </div>  
@@ -126,6 +172,9 @@
         </div>           
     </div>
 
+    <div id="apDivRating">
+
+    </div>
 	
 	
     <div id="calendarBox">
