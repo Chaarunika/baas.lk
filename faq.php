63078b7352c05c7c76be8f741632199b74eedd1c
Mkavishan <miyurukavishan@gmail.com>
2016-01-12 11:07:58 -0800

    find job comment

--- faq.php
+++ faq.php
@@ -4,24 +4,23 @@
 <head>
 	<title>baas.lk</title>
 	<meta charset="UTF-8">
-
 <link href="css/header.css" rel="stylesheet">
 
 <style>
-@import "http://fonts.googleapis.com/css?family=Fauna+One|Muli";
-p{	
-	color: rgba(51,51,51,1);
-	font-size:14px;
-	font-family:'Fauna One',serif;
-	padding-left: 5px;
+	@import "http://fonts.googleapis.com/css?family=Fauna+One|Muli";
+	p{	
+		color: rgba(51,51,51,1);
+		font-size:14px;
+		font-family:'Fauna One',serif;
+		padding-left: 5px;
 	}
-h1{	
-	font-family:'Fauna One',serif
-}
-h3{	
-	font-family:'Fauna One',serif;
-}
- .FAQ { 
+	h1{	
+		font-family:'Fauna One',serif
+	}
+	h3{	
+		font-family:'Fauna One',serif;
+	}
+ 	.FAQ { 
  		cursor:hand; cursor:pointer;
         border:1px solid darkorange;
         border-radius: 3px;
@@ -31,81 +30,81 @@ h3{
        // padding :5px;  	       
     }
 
- .FAA { 
- 	display:none;
- 	font-size: 10px;
+ 	.FAA { 
+ 		display:none;
+ 		font-size: 10px;
 
- 	 }
-#apDivTitle{
-	position: absolute;
-	left: 131px;
-	top: 90px;
-	width: 70%;
-	height: 105px;
-	z-index: 111;
-	font-size: 10px;
-}
-#apDivQuestionBox {
-	position: absolute;
-	left: 800px;
-	top: 170px;
-	width: 390px;
-	height: 350px;
-	z-index: 1;
-	background-color: #f0f0f0;
-	border-radius: 5px;
-	border:1px solid rgba(51,51,51,0.5);
-}
-#apDivQuestionTitle {
-	position: absolute;
-	left: 16px;
-	top: 18px;
-	width: 350px;
-	z-index: 2;
-}
+ 	}
+	#apDivTitle{
+		position: absolute;
+		left: 131px;
+		top: 90px;
+		width: 70%;
+		height: 105px;
+		z-index: 111;
+		font-size: 10px;
+	}
+	#apDivQuestionBox {
+		position: absolute;
+		left: 800px;
+		top: 170px;
+		width: 390px;
+		height: 350px;
+		z-index: 1;
+		background-color: #f0f0f0;
+		border-radius: 5px;
+		border:1px solid rgba(51,51,51,0.5);
+	}
+	#apDivQuestionTitle {
+		position: absolute;
+		left: 16px;
+		top: 18px;
+		width: 350px;
+		z-index: 2;
+	}
 
-#apDivQustionForum {
-	position: absolute;
-	left: 19px;
-	top: 155px;
-	width: 340px;
-	height: 147px;
-	z-index: 2;
-}
-.post{
-width:100%;
-height:40px;
-margin-top:15px;
-margin-bottom:15px;
-background-color:rgba(240,240,240,0.8);
-border:1px solid rgba(0,0,0,0.2);
-border-radius:3px;
-font-family:'Fauna One',serif;
-font-weight:500;
-font-size:14px;
-border-style: solid;
-border-width: 1px;
-}
-.post:hover{
-	background-color:#a6a6a6;
-}
-.faqText{
-width:100%;
-height:60px;
-margin-top:10px;
-border:none;
-border-radius:3px;
-padding:5px;
-resize:none;
-border-style: solid;
-border-width: 1px;
-border-color: rgba(240,240,1,1);
-font-size: 14px;
-}
+	#apDivQustionForum {
+		position: absolute;
+		left: 19px;
+		top: 155px;
+		width: 340px;
+		height: 147px;
+		z-index: 2;
+	}
+	.post{
+		width:100%;
+		height:40px;
+		margin-top:15px;
+		margin-bottom:15px;
+		background-color:rgba(240,240,240,0.8);
+		border:1px solid rgba(0,0,0,0.2);
+		border-radius:3px;
+		font-family:'Fauna One',serif;
+		font-weight:500;
+		font-size:14px;
+		border-style: solid;
+		border-width: 1px;
+	}
+	.post:hover{
+		background-color:#a6a6a6;
+	}
+	.faqText{
+		width:100%;
+		height:60px;
+		margin-top:10px;
+		border:none;
+		border-radius:3px;
+		padding:5px;
+		resize:none;
+		border-style: solid;
+		border-width: 1px;
+		border-color: rgba(240,240,1,1);
+		font-size: 14px;
+	}
 
-label{
-	font-size: 14px;
-}
+	label{
+		font-size: 14px;
+	}
 </style>
 
 <script type="text/javascript">
@@ -118,7 +117,6 @@ label{
 </head>
 
 <body>
-
 
 <?php
  include('header.php');
