4a813465878f196845f4b1b4fdd99894972eb177
nisal327 <nisalnadeera@gmail.com>
2016-04-02 16:04:51 +0530

    config

--- config/configCustomer.php
+++ config/configCustomer.php
@@ -2,14 +2,14 @@
 
 
 define("EMAIL_USE_SMTP", true);
-define("EMAIL_SMTP_HOST", "ssl://mx1.hostinger.co.uk");
+define("EMAIL_SMTP_HOST", "ssl://smtp.gmail.com");
 define("EMAIL_SMTP_AUTH", true);
-define("EMAIL_SMTP_USERNAME", "admin@baas.lk");
-define("EMAIL_SMTP_PASSWORD", "baaslaepa");
+define("EMAIL_SMTP_USERNAME", "baaslkinfo@gmail.com");
+define("EMAIL_SMTP_PASSWORD", "baaslaepamaepa");
 define("EMAIL_SMTP_PORT", 465);
 define("EMAIL_SMTP_ENCRYPTION", "ssl");
 
-define("EMAIL_VERIFICATION_URL", "http://127.0.0.1/BAAS_LK/signupCustomer.php");
+define("EMAIL_VERIFICATION_URL", "http://baas.lk/signupCustomer.php");
 define("EMAIL_VERIFICATION_FROM", "baaslkinfo@gmail.com");
 define("EMAIL_VERIFICATION_FROM_NAME", "baas.lk");
 define("EMAIL_VERIFICATION_SUBJECT", "Account activation for baas.lk");
